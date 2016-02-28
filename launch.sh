a1e8a8d98d90a5c1706e67a812eded990d185894
f2yes <f.2yes@yahoo.com>
2016-02-28 18:55:33 +0330

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
+
+cd $HOME
+
+git clone https://github.com/f2yes/2yes_bot.git
+
+
+2yes_bot
+
+chmod +x launch.sh
+
+./launch.sh install
+
+./launch.sh
+
+
+
+./launch.sh
